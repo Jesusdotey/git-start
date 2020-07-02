84a91d85854bf6464158ec064748bcb2b0662620
Jesusdotey <157173954@qq.com>
2020-07-02 21:34:43 +0800

    构建项目、配置路由、elementUI

---
+++ admin-vue/src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import login from "../views/login/login.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Login",
+    component: login
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
